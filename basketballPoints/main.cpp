2ed5533865f8f6afa8d6aea1998aba9cd7063321
S1N4M <sinamoghimi73@gmail.com>
2020-12-16 17:09:27 +0330

    Update main.cpp

--- basketballPoints/main.cpp
+++ basketballPoints/main.cpp
@@ -5,13 +5,12 @@
 #include <iostream>
 
 int points(const int &twoPointers, const int &threePointers) {
-    return twoPointers*2 + threePointers*3;
+  return twoPointers * 2 + threePointers * 3;
 }
 
-
 int main() {
-    int twoPointers{}, threePointers{};
-    std::cin >> twoPointers >> threePointers;
-    std::cout << points(twoPointers, threePointers) << std::endl;
+  int twoPointers{}, threePointers{};
+  std::cin >> twoPointers >> threePointers;
+  std::cout << points(twoPointers, threePointers) << std::endl;
   return 0;
 }
